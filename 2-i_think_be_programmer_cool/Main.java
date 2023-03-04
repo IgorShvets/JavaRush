dc27e4ae6ee1976eec053b0f25c9a202be456498
shiyacode <82521702+IgorShvets@users.noreply.github.com>
2023-03-04 23:25:43 +0300

    вторая - третья попытка )))

---
+++ 2-i_think_be_programmer_cool/Main.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0101;
+
+/*
+многострочный коммент
+ */
+
+public class Main {
+    public static void main (String[] args){
+        System.out.print ("Я думаю");
+        System.out.print (", быть программистом круто");
+        //однострочный коммент
+    }
+}
