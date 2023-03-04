cffced45ce2037b3017633000e6c3ee49344c05a
shiyacode <82521702+IgorShvets@users.noreply.github.com>
2023-03-04 23:35:18 +0300

    what happens )))

--- amigo_very_smart_1_1/Main.java
+++
@@ -1,7 +0,0 @@
-package com.javarush.task.task01.task0142;
-public class Main {
-    public static void main (String[] args) {
-        //они просят написать код тут, так и сделаем
-        System.out.println ("Амиго очень умный");
-    }
-}
