ffa7cb0032f2e1092c6c80e110be305c151c6329
shiyacode <82521702+IgorShvets@users.noreply.github.com>
2023-03-05 12:22:56 +0300

    5 мой юнный друг

---
+++ 5-мой_юнный_друг/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public static void main (String[] args) {
+        //как всегда однострочный комментарий
+        int friendBerthday = 3126 - 8;
+        System.out.println (friendBerthday);
+    }
+}
