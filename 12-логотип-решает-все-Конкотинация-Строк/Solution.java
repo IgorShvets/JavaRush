a882b96306a19d876445ee000a57dd48391f6bbb
Igot Shvets <me@shiyacode.ru>
2023-03-21 17:48:26 +0300

    конкотинация строк

---
+++ 12-логотип-решает-все-Конкотинация-Строк/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        String s1 = "Do";
+        String s2 = "It";
+        String s3 = "Just";
+
+        String tagline = s3+s1+s2; //напишите тут ваш код
+
+        System.out.println(tagline);
+    }
+}
