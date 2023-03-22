d444ecc67b8c584e5777c4ccc6d8f3852786d5b3
Igot Shvets <me@shiyacode.ru>
2023-03-22 11:57:18 +0300

    повышение регистра String toUpperCase()

---
+++ 18-прошу-регистр-на-меня-не-повышать-toUpperCase()-повышение-регистра/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        String caps = "if I type in caps ";
+        String usa = "сша";
+
+        //напишите тут ваш код
+        System.out.println(usa.toUpperCase());
+        System.out.println("Винни Пух".toUpperCase());
+        System.out.println((caps + "they know I mean business").toUpperCase());
+    }
+}
